b9ccb41eed61687629c2f6c3a101adc574cbbd95
ScripterXGC <angelojudeserranothe2nd@gmail.com>
2025-01-05 14:41:40 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Function to redirect to the bot invite link
+function inviteBot() {
+    // Replace the link with your actual bot invite URL
+    const inviteUrl = "https://discord.com/oauth2/authorize?client_id=1323840372371099720&permissions=1759218604441591&integration_type=0&scope=bot";
+    window.location.href = inviteUrl;
+}
