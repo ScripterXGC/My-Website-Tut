ee9f06bb7202a82d49889079bc8faa3ddb9498da
ScripterXGC <angelojudeserranothe2nd@gmail.com>
2025-01-06 17:55:07 +0800

    Ooo

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+function server() {
+	const weblink = 'https://discord.gg/2qstUBX5cv'
+	window.open(weblink);
+}
+
+function invite() {
+	const weblink = "https://discord.com/oauth2/authorize?client_id=1323840372371099720&permissions=1759218604441591&integration_type=0&scope=bot"
+	window.open(weblink);
+}
